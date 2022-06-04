8b95f5adca93121870d19b09dfb8b77fd101e875
TonyNogueron <antonio.nogueron03@gmail.com>
2022-06-04 14:53:32 -0500

    Added html elements and linked them to js script

--- Frontend/js/sketch.js
+++ Frontend/js/sketch.js
@@ -2,64 +2,22 @@ points = [];
 measurements = [];
 var img;
 
-var referencia;
-var myImg;
-var calcular;
-var ss;
-var correcto;
-var reset;
-var paragraph;
 var myCanvas;
+
+const REFERENCIA = 9;
+const DIVWIDTH = document.querySelector('#prueba').offsetWidth;
 
 let pixel_2_cm_ratio;
 let longitud_calculada;
 
-
-
-//function imgSize(){
-//       myImg = document.querySelector("#caradelmau");
-//        var currWidth = myImg.clientWidth;
-//        var currHeight = myImg.clientHeight;
-//00        return [currWidth, currHeight]
-//}
-
 function setup() {
     img = createCapture(VIDEO);
     img.hide();
-    //const [ancho, alto] = imgSize();
-    myCanvas = createCanvas(293, 180);
+    myCanvas = createCanvas(DIVWIDTH, DIVWIDTH*0.6);
     myCanvas.parent("prueba");
     myCanvas.position(0,0);
     
     image(img, 0, 0, width, height);
-    
-    referencia = createInput("9");
-    referencia.parent("prueba");
-    referencia.position(0,190);
-    
-    ss = createButton("Take Photo");
-    ss.parent("prueba");
-    ss.mousePressed(screenshot);
-    ss.position(0,220);
-
-    calcular = createButton("Calcular");
-    calcular.parent("prueba");
-    calcular.mousePressed(calcula);
-    calcular.position(0,250);
-
-    reset = createButton("Reiniciar");
-    reset.parent("prueba");
-    reset.mousePressed(reiniciar);
-    reset.position(0,280);
-
-    correcto = createButton("Correcto");
-    correcto.parent("prueba");
-    correcto.mousePressed(guarda);
-    correcto.position(0,310);
-
-    paragraph = createP("Waiting for measurement...").position(0,350);
-    paragraph.style("font-size","25px");
-    paragraph.parent("prueba");
 }
 
 function draw() {
@@ -80,31 +38,35 @@ function draw() {
         }
     }
 }
+document.getElementById("screenshot").addEventListener("click",screenshot);
+document.getElementById("reset").addEventListener("click",reiniciar);
+document.getElementById("calculate").addEventListener("click",calcula);
+document.getElementById("correct").addEventListener("click",guarda);
 
 function reiniciar() {
     points = [];
-    paragraph.html("Waiting for measurement...");
+    document.getElementById("measurement-text").innerHTML = "Waiting for measurement...";
     img = createCapture(VIDEO);
     img.hide();
 }
+
+
 function screenshot(){
     img = myCanvas.get();
-
 }
 
 function guarda(){
     if(longitud_calculada != 0){
     append(measurements, longitud_calculada);
-    paragraph.html("Measurement has been saved correctly.");
+    document.getElementById("measurement-text").innerHTML = "Measurement has been saved correctly.";
     }
 }
 
 function calcula() {
-    if (points.length % 2 == 0 && points.length >= 8 && referencia.value() != "") {
-        pixel_2_cm_ratio = Math.sqrt(Math.pow(points[2] - points[0], 2) + Math.pow(points[3] - points[1], 2)) / float(referencia.value());
+    if (points.length % 2 == 0 && points.length >= 8) {
+        pixel_2_cm_ratio = Math.sqrt(Math.pow(points[2] - points[0], 2) + Math.pow(points[3] - points[1], 2)) / float(REFERENCIA);
         longitud_calculada = Math.sqrt(Math.pow(points[6] - points[4], 2) + Math.pow(points[7] - points[5], 2)) / float(pixel_2_cm_ratio);
-        //createP("La longitud Calculada es:" + str(longitud_calculada) + "cm");
-        paragraph.html("La longitud Calculada es: " + str(longitud_calculada.toFixed(4)) + " cm");
+        document.getElementById("measurement-text").innerHTML = "Measurement: " + str(longitud_calculada.toFixed(4)) + " cm";
     }
 }
 
