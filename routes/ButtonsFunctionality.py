2dc71b46dbf8fd822a79dba753c7cafe11f3577c
Leo Perez <leoperez3006@gmail.com>
2022-06-11 11:11:13 -0500

    serial comunication in action buttons

--- routes/ButtonsFunctionality.py
+++ routes/ButtonsFunctionality.py
@@ -1,5 +1,12 @@
 import json
 from flask import Flask, request, Blueprint
+import serial
+
+arduino = serial.Serial(port='usb-1a86_USB_Serial-if00-port0', baudrate=115200, timeout=1)
+
+
+def send(message):
+    arduino.write(bytes(message, 'UTF-8'))
 
 
 buttons_functionality = Blueprint('buttons_functionality', __name__)
@@ -7,4 +14,5 @@ buttons_functionality = Blueprint('buttons_functionality', __name__)
 @buttons_functionality.route('actuators', methods=['POST'])
 def send_actions():
     json = request.get_json()
-    print(json )
+    send(json.actions)
+    # print(json )
