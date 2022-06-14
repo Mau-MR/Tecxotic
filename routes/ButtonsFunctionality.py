e5606d6d628735ad637284bee31c48cfe7169580
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-06-14 10:21:37 -0500

    Fixed serial route

--- routes/ButtonsFunctionality.py
+++ routes/ButtonsFunctionality.py
@@ -2,8 +2,7 @@ import json
 from flask import Flask, request, Blueprint
 import serial
 buttons_functionality = Blueprint('buttons_functionality', __name__)
-
-arduino = serial.Serial(port='usb-1a86_USB_Serial-if00-port0', baudrate=115200, timeout=1)
+arduino = serial.Serial(port='/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', baudrate=115200, timeout=1)
 # arduino = serial.Serial(port='COM3', baudrate=115200, timeout=1) #for testing in local 
 
 
