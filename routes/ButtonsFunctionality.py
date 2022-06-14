09a59c7082958b431bfd084796b300353bb94829
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-06-14 12:13:24 -0500

    Catch exeption from arduino serial

--- routes/ButtonsFunctionality.py
+++ routes/ButtonsFunctionality.py
@@ -2,8 +2,11 @@ import json
 from flask import Flask, request, Blueprint
 import serial
 buttons_functionality = Blueprint('buttons_functionality', __name__)
-arduino = serial.Serial(port='/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', baudrate=115200, timeout=1)
-# arduino = serial.Serial(port='COM3', baudrate=115200, timeout=1) #for testing in local 
+try:
+    arduino = serial.Serial(port='/dev/serial/by-id/usb-1a86_USB_Serial-if00-port0', baudrate=115200, timeout=1)
+except Exception as e:
+    print("ERROR in ButtonsFunctionality.py, serial route not founded: " + str(e))
+# arduino = serial.Serial(port='COM3', baudrate=115200, timeout=1) #for testing in local
 
 
 def send(message):
