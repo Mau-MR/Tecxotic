d8a8edca9dd82f1eb174c784f44d013d11161ec8
Mau-MR <46287549+Mau-MR@users.noreply.github.com>
2022-06-23 15:56:06 -0500

    Capture creation validation

--- Capture.py
+++ Capture.py
@@ -1,3 +1,5 @@
+import sys
+
 import cv2
 class Capture:
     def __init__(self, source=0):
@@ -8,8 +10,15 @@ class Capture:
             cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
             cap.set(cv2.CAP_PROP_FPS, 30)
             self.cap = cap
+            ret, frame = self.cap.read()
+            if not ret:
+                raise Exception('Could not get frame of capture', source)
         except Exception as e:
-            print("Error in Capture.py: Unable to open capture", str(e))
+            print("Error in Capture.py: ", str(e))
+            sys.exit(0)
+            return
+        print("Sucessfully opened capture with id", source)
+
 
     #TODO: HANDLE THE ERROR WITH NO FRAME
     def get_frame(self):
